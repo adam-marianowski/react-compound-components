cc64190405ee994762a462a56f65b9ef66805c3f
Adam Marianowski <a.marainowski@outlook.com>
2024-07-31 20:13:10 +0200

    refactored components to arrow fns

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useState } from "react";
-import PropTypes from "prop-types";
 
 const AccordionContext = createContext();
 const useAccordionContext = () => useContext(AccordionContext);
@@ -7,9 +6,9 @@ const useAccordionContext = () => useContext(AccordionContext);
 const AccordionItemContext = createContext();
 const useAccordionItemContext = () => useContext(AccordionItemContext);
 
-export default function Accordion(props) {
+const Accordion = props => {
   const [openItemId, setOpenItemId] = useState();
-  const toggle = (id) => setOpenItemId((p) => (p === id ? null : id));
+  const toggle = id => setOpenItemId(p => (p === id ? null : id));
   const contextValue = { openItemId, toggle };
 
   return (
@@ -17,29 +16,39 @@ export default function Accordion(props) {
       <ul>{props.children}</ul>
     </AccordionContext.Provider>
   );
-}
+};
 
-function AccordionTitle(props) {
+const AccordionTitle = props => {
   const id = useAccordionItemContext();
   const ctx = useAccordionContext();
   return <h3 onClick={() => ctx.toggle(id)}>{props.children}</h3>;
-}
+};
 
-function AccordionContent(props) {
+const AccordionContent = props => {
   const ctx = useAccordionContext();
   const id = useAccordionItemContext();
   const className = ctx.openItemId === id ? "" : "hidden";
-  return <div className={className} style={{display: ctx.openItemId === id? "" : "none"}}>{props.children}</div>;
-}
 
-function AccordionItem(props) {
+  return (
+    <div
+      className={className}
+      style={{ display: ctx.openItemId === id ? "" : "none" }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+const AccordionItem = props => {
   return (
     <AccordionItemContext.Provider value={props.id}>
       <li>{props.children}</li>
     </AccordionItemContext.Provider>
   );
-}
+};
 
 Accordion.Item = AccordionItem;
 Accordion.Title = AccordionTitle;
 Accordion.Content = AccordionContent;
+
+export default Accordion;
